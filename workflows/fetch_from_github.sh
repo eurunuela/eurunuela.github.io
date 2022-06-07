6ab4afcc48e54798405b4546e6c14258b67973a5
Eneko Uruñuela <e.urunuela@icloud.com>
2022-06-07 14:17:44 -0400

    Delete fetch_from_github.sh

--- workflows/fetch_from_github.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl -L -X POST 'https://api.github.com/graphql' \
--H 'Authorization: bearer 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' \
---data-raw '{"query":"{\n  user(login: \"eurunuela\") {\n pinnedItems(first: 6) {\n totalCount\n edges {\n node {\n ... on Repository {\n name\n id\n url\n stargazers {\n totalCount\n }\n description\n }\n }\n }\n }\n }\n }"' \
--o github_data.json
-# {\n  user(login: \"eurunuela\") {\n pinnedItems(first: 6) {\n totalCount\n edges {\n node {\n ... on Repository {\n name\n id\n url\n stargazers {\n totalCount\n }\n description\n }\n }\n }\n }\n }\n }
