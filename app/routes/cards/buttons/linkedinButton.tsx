65f4236481e84da52ac63309cd270706eb41a35b
Eneko Uruñuela <e.urunuela@icloud.com>
2022-06-08 11:05:29 -0400

    Update link to linkedin profile

--- app/routes/cards/buttons/linkedinButton.tsx
+++ app/routes/cards/buttons/linkedinButton.tsx
@@ -4,7 +4,7 @@ export default function LinkedinButton() {
   return (
     <div>
       <a
-        href="https://www.twitter.com/eurunuela"
+        href="https://www.linkedin.com/in/eneko-urunuela/"
         target="_blank"
         className="flex justify-center items-center py-3 px-10 rounded-md w-full h-auto bg-gray-100 text-gray-700 hover:decoration-none hover:bg-gray-200 hover:text-gray-700 hover:no-underline"
       >
